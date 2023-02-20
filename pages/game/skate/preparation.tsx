e818c60cc63df77c96226d0c22282c6dbe175c17
benbenpetit <petitbenoit3@gmail.com>
2023-02-20 11:16:37 +0100

    feature/skate-interface : init pages

---
+++ pages/game/skate/preparation.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next'
+
+const SkatePreparationPage: NextPage = () => {
+  return <div>Preparation</div>
+}
+
+export default SkatePreparationPage
---
+++ pages/game/skate/trial.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next'
+
+const SkateTrialPage: NextPage = () => {
+  return <div>Trial</div>
+}
+
+export default SkateTrialPage
