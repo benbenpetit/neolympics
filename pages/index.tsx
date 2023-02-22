5e6f09e602b52952056eec532f072939d6388e96
benbenpetit <petitbenoit3@gmail.com>
2023-02-22 16:38:05 +0100

    feature/skate-interface : home screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
 import BaseButton from '@/components/common/Button/BaseButton'
+import Button from '@/components/common/Button/Button'
 import { NextPage } from 'next'
+import { useRouter } from 'next/router'
 
 const Home: NextPage = () => {
+  const router = useRouter()
+
   return (
-    <>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel, iusto!</p>
-      <BaseButton label='Click' fullWidth />
-    </>
+    <div className='c-home o-container --vertical u-align-center u-justify-center'>
+      <h1 className='c-home__title c-heading --giant u-white'>Neolympics</h1>
+      <p className='c-home__paragraph'>
+        Découvrez les sports additionnels des Jeux Olympiques de 2024 et battez-vous pour
+        la médaille !
+      </p>
+      <Button variant='secondary' onClick={() => router.push('/game/skate/preparation')}>
+        Commencer l'expérience
+      </Button>
+    </div>
   )
 }
 
