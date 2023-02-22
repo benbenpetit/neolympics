0b6ba40ee2957dbf53a9b9e2fd5c0ea107965bd6
arthurch3 <116687588+arthurch3@users.noreply.github.com>
2023-02-22 18:28:37 +0100

    add popup interaction

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -4,6 +4,8 @@ import NavbarButton from './NavbarButton'
 import Image from 'next/image'
 import volumeIcon from '@/public/img/sound-on.svg'
 import muteIcon from '@/public/img/sound-off.svg'
+import Popup from '@/components/common/Navbar/Popup'
+import Button from '@/components/common/Button/Button'
 
 interface Props {
   className?: string
@@ -11,31 +13,58 @@ interface Props {
 
 const Navbar: FC<Props> = ({ className }) => {
   const [isVolume, setIsVolume] = useState(true)
-  const click2 = () => {
-    console.log('accueil clicked')
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const accueilClick = () => {
+    setIsModalOpen(true)
+  }
+
+  const continueClick = () => {
+    setIsModalOpen(false)
+  }
+
+  const exitClick = () => {
+    console.log('EXIT')
+    setIsModalOpen(false)
   }
 
   return (
-    <div className={clsx('c-navbar', className)}>
-      <NavbarButton handleClick={() => setIsVolume(!isVolume)}>
-        <Image
-          src={isVolume ? volumeIcon : muteIcon}
-          alt='logo'
-          height='15'
-          width='15'
-          className='o-picto'
-        />
-      </NavbarButton>
-      <NavbarButton handleClick={click2}>
-        <Image
-          src={'/img/logo-jo.svg'}
-          alt='logo'
-          height='15'
-          width='15'
-          className='o-picto'
-        />
-        <span>Accueil</span>
-      </NavbarButton>
+    <div className='c-nav'>
+      <div className={clsx('c-navbar', className)}>
+        <NavbarButton handleClick={() => setIsVolume(!isVolume)}>
+          <Image
+            src={isVolume ? volumeIcon : muteIcon}
+            alt='logo'
+            height='15'
+            width='15'
+            className='o-picto'
+          />
+        </NavbarButton>
+        <NavbarButton handleClick={accueilClick}>
+          <Image
+            src={'/img/logo-jo.svg'}
+            alt='logo'
+            height='15'
+            width='15'
+            className='o-picto'
+          />
+          <span>Accueil</span>
+        </NavbarButton>
+      </div>
+      {isModalOpen && (
+        <Popup label='Abandonner la compétition ?'>
+          Retourner à l’accueil vous fera abandonner votre épreuve actuelle. <br /> Êtes
+          vous sur de vouloir quitter ?
+          <div className='c-popup__button-wrapper'>
+            <Button onClick={exitClick} variant='secondary'>
+              Oui
+            </Button>
+            <Button onClick={continueClick} variant='secondary'>
+              Non
+            </Button>
+          </div>
+        </Popup>
+      )}
     </div>
   )
 }
--- pages/game/skate/trial.tsx
+++ pages/game/skate/trial.tsx
@@ -1,34 +1,16 @@
 import Navbar from '@/components/common/Navbar/Navbar'
 import Background from '@/components/module/Games/Skate/Background'
 import TrialFooter from '@/components/module/Games/Skate/TrialFooter'
-import Popup from '@/components/common/Navbar/Popup'
-import Button from '@/components/common/Button/Button'
 
 import { NextPage } from 'next'
 import { useState } from 'react'
 
 const TrialPage: NextPage = () => {
-  const [isModalOpen, setIsModalOpen] = useState(true)
-
   return (
     <div>
       <Navbar />
       <Background />
       <TrialFooter />
-      {isModalOpen && (
-        <Popup label='Abandonner la compétition ?'>
-          Retourner à l’accueil vous fera abandonner votre épreuve actuelle. <br /> Êtes
-          vous sur de vouloir quitter ?
-          <div className='c-popup__button-wrapper'>
-            <Button onClick={() => setIsModalOpen(false)} variant='secondary'>
-              Oui
-            </Button>
-            <Button onClick={() => setIsModalOpen(false)} variant='secondary'>
-              Non
-            </Button>
-          </div>
-        </Popup>
-      )}
     </div>
   )
 }
