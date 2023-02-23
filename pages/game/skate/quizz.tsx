25755188fbe27909d0b400e0034f3587a8b2620a
Theotime Lameyre <theotime.lameyre@gmail.com>
2023-02-23 10:42:07 +0100

    quizz init

---
+++ pages/game/skate/quizz.tsx
@@ -0,0 +1,13 @@
+import Navbar from '@/components/common/Navbar/Navbar'
+import { NextPage } from 'next'
+
+const SkateQuizzPage: NextPage = () => {
+  return (
+    <div className='c-quizz'>
+      <Navbar />
+      <h1>YOOOO</h1>
+    </div>
+  )
+}
+
+export default SkateQuizzPage
