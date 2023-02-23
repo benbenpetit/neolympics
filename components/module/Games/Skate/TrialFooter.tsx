8a1456ff965748b63de2997deea9ceabfc45c468
benbenpetit <petitbenoit3@gmail.com>
2023-02-23 17:06:05 +0100

    feature/skate-interface : popup kickflip

--- components/module/Games/Skate/TrialFooter.tsx
+++ components/module/Games/Skate/TrialFooter.tsx
@@ -1,23 +1,15 @@
 import ProgressBar from '@/components/module/Games/Skate/ProgressBar'
 import Score from '@/components/module/Games/Skate/Score'
-import Timecode from '@/components/module/Games/Skate/Timecode'
 import clsx from 'clsx'
-import { FC, useEffect, useState } from 'react'
+import { FC } from 'react'
 
 interface Props {
+  time: number
+  totalTime: number
   className?: string
 }
 
-const TrialFooter: FC<Props> = ({ className }) => {
-  const [time, setTime] = useState(0)
-  const totalTime = 45
-
-  useEffect(() => {
-    if (time < totalTime) {
-      setTimeout(() => setTime(time + 0.1), 100)
-    }
-  }, [time])
-
+const TrialFooter: FC<Props> = ({ time, totalTime, className }) => {
   return (
     <div className={clsx('c-trial-footer', className)}>
       <div className='c-trial-footer__progress'>
--- pages/game/skate/trial.tsx
+++ pages/game/skate/trial.tsx
@@ -1,16 +1,49 @@
 import Navbar from '@/components/common/Navbar/Navbar'
 import Background from '@/components/module/Games/Skate/Background'
 import TrialFooter from '@/components/module/Games/Skate/TrialFooter'
-
 import { NextPage } from 'next'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
+import Image from 'next/image'
 
 const TrialPage: NextPage = () => {
+  const [time, setTime] = useState(0)
+  const totalTime = 45
+
+  useEffect(() => {
+    if (time < totalTime) {
+      setTimeout(() => setTime(time + 0.1), 100)
+    }
+  }, [time])
+
+  const shouldDraw = () => {
+    return time > 1 && time <= 16
+  }
+
   return (
     <div>
       <Navbar />
       <Background />
-      <TrialFooter />
+      <AnimatePresence>
+        {shouldDraw() && (
+          <motion.div
+            key='key'
+            initial={{ opacity: 0, x: '-50%', y: '-40%' }}
+            animate={{ opacity: 1, y: '-50%' }}
+            exit={{ opacity: 0, y: '-40%' }}
+            transition={{ duration: 0.3, ease: [0.4, 0, 0, 1] }}
+            className='c-skate-popup'
+          >
+            <Image
+              src={'/img/skate/figures/figure-kickflip.gif'}
+              alt='Schema animé du Kickflip'
+              width={1000}
+              height={800}
+            />
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <TrialFooter time={time} totalTime={totalTime} />
     </div>
   )
 }
